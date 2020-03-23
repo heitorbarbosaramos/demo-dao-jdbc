d2134c98e6faf2d1bd1c393c0faed8767206083b
Heitor Ramos <heitorhfbr@gmail.com>
2020-03-23 00:51:33 -0300

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,13 +1,20 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department department = new Department(1,"Objeto");
+		Department department = new Department(1,"books");
 		System.out.println(department);
+		
+		Seller seller = new Seller(2, "Bob", "bob@email.com", new Date(), 2000.0, department);
+		
+		System.out.println(seller);
 	}
 
 }
